0c64c48eeb0184c29732ef89b0bfff620b088b9f
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-03-22 11:59:22 +0200

    Add Welcome Controller

---
+++ ECommerce512/Controllers/WelcomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce512.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            // Data
+            string name = "Mohamed Ashraf";
+            int age = 15;
+            double salary = 5000;
+
+            var person = new { name, age, salary };
+
+            return View(model: person);
+        }
+    }
+}
