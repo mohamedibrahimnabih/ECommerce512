05132128195d60264925381226c448c1669b440f
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-03-22 14:16:06 +0200

    Configure DB & Change Layout

---
+++ ECommerce512/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce512.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce512.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=ECommerce512;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+    }
+}
