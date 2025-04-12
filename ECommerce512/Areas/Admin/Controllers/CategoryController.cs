555087e73f13036cfca317083c687374020827aa
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-04-12 12:12:09 +0200

    Create Operation

---
+++ ECommerce512/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce512.Data;
+using ECommerce512.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce512.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context = new();
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories;
+
+            return View(categories.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
