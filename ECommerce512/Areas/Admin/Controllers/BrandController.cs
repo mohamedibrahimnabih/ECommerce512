65ecc83eecaa035f3d0e32eb6c6ebc0183a6774e
eslam <eslam3400@gmail.com>
2025-04-12 13:07:02 +0200

    update

---
+++ ECommerce512/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce512.Data;
+using ECommerce512.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce512.Areas.Admin.Controllers
+{
+    public class BrandController : Controller
+    {
+        private readonly ApplicationDbContext _context = new();
+
+        public IActionResult Index()
+        {
+            var Brands = _context.Brands;
+
+            return View(Brands.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Brand brand)
+        {
+            _context.Brands.Add(brand);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+
